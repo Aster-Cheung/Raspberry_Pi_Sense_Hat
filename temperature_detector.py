450e7dcc5bf2fa672d03c45fa5c008c67bbf35ac
Aster Cheung <67261899+Aster-Cheung@users.noreply.github.com>
2026-08-08 17:09:58 -0400

    Update temperature_detector.py

--- temperature_detector.py
+++ temperature_detector.py
@@ -3,8 +3,11 @@ from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
 
+
+amber = (255, 100, 0)
+
 temperature = sense.get_temperature()
 temperature = round(temperature, 2)
 print(temperature)
 
-sense.show_message(str(temperature)+ " C" )
+sense.show_message(str(temperature)+ " C", text_colour = amber )
