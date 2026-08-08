8b2fb4b8bb62c710e3847fd270768c628dd6c800
Aster Cheung <67261899+Aster-Cheung@users.noreply.github.com>
2026-08-08 17:11:04 -0400

    Update letter.py
    Added Color

--- letter.py
+++ letter.py
@@ -1,4 +1,8 @@
 from sense_hat import SenseHat
+
 MySH = SenseHat()
+#MySH.low_light = True
+
+green = (0,25, 0)
 a = input('Enter a letter:')
-MySH.show_letter(a)
+MySH.show_letter(a, text_colour = green)
